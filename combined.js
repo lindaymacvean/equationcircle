8cb48d37137e2d2625c00fff3a1dcc12be2e11ef
Lindsay Macvean <lindsaymacvean@gmail.com>
2012-12-15 16:40:30 +0000

    drawing a circle

--- combined.js
+++ combined.js
@@ -3,7 +3,22 @@ $(document).ready(function(){
 var canvas = document.getElementById("canvas1");
 var ctx = canvas.getContext('2d');
 
-c.width = document.body.clientWidth;
-c.height = document.body.clientHeight;
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+
+//draw circle
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
+var radius = 70;
+
+context.beginPath();
+context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+context.fillStyle = 'green';
+context.fill();
+context.lineWidth = 5;
+context.strokeStyle = '#003300';
+context.stroke();
+
+//draw hole
 
 });
