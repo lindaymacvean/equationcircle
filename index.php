eabb9e8eaa9deefe24e0770c67ca9df2c9b14f68
Alan Panayotov <alanpanayotov@gmail.com>
2013-01-03 04:58:42 +1200

    added randomness
    fixed r, converted to php, added warning of browser compatibility, a LOT
    more

---
+++ index.php
@@ -0,0 +1,10 @@
+<script src="assets/jquery-1.7.1.min.js"></script>
+<span>
+If you have Internet Explorer switch to Chrome or Firefox to continue the evolution of the internet.<br>Thank you.
+</span>
+<form id='redirect' action='game.php' method='post'>
+	<input type="hidden" id="score" name="score" value="0" />
+	<input type="submit" id="redirect" value="Start Game!"/>
+</form>
+<script>
+</script>
